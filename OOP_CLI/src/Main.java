a6b1942ab518f8d05879d465614b7abea3cdd37b
Tharana Bopearachchi <tharanabope30@gmail.com>
2024-12-08 13:06:53 +0530

    Created the Main class with the Basic

--- OOP_CLI/src/Main.java
+++ OOP_CLI/src/Main.java
@@ -6,9 +6,9 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Real-Time Ticketing System!");
 
-        // Placeholder for system setup
+
         System.out.println("System setup in progress...");
 
-        // TODO: Add configuration handling, TicketPool, and threading logic in subsequent steps.
+
     }
 }
